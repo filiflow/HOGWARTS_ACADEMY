7fea7ca77b730d917b04a825af3756498f8e3958
Florian Berthouly <berthouly.florian@gmail.com>
2023-05-30 18:17:01 +0200

    new form & modifications on controller activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,9 +1,32 @@
 class ActivitiesController < ApplicationController
+  # before_action :authenticate_user!, only: [:new, :create]
   def index
     @activities = Activity.all
   end
 
   def show
     @activity = Activity.find(params[:id])
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+    @activity.id = current_user
+    @activity.save
+    raise
+    if @activity.save
+      redirect_to activity_path(@activity)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:title, :theme, :price, :date, :summary, :description)
   end
 end
--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,20 +2,4 @@ class DashboardsController < ApplicationController
   def index
     @activities = Activity.where(user: current_user)
   end
-
-  def new
-    @activity = Activity.new
-  end
-
-  def create
-    @activity = Activity.new(activity_params)
-    @activity.save
-    redirect_to dashboards_path
-  end
-
-  private
-
-  def activity_params
-    params.require(:activity).permit(:title, :theme, :price, :date)
-  end
 end
