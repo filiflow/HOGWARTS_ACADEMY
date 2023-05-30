f2dc37b53727aae3aca70b88ffae4ea6759de701
Marianne Brandely <marianne.brandely@gmail.com>
2023-05-30 18:17:18 +0200

    navbar links + dashboard index

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "activities#index"
   resources :activities, only: %i[show new create]
-  # get "activities/:id",   to: "activities#show"
-  # get "activities/new",   to: "activities#new"
-  # post "activites",       to: "activities#create"
-  # get "bookings/new" to: "bookings#new"
-  # post "bookings", to: "bookings#create"
-  get "dashboards", to: "dashboards#index"
+  resources :dashboards, only: %i[index new create]
 end
